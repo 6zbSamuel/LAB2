f1c5ed02220d18d54746f25c2a5aa716ef7c16fc
6zbSamuel <134755875+6zbSamuel@users.noreply.github.com>
2024-10-30 22:08:14 +0800

    Update StudentService.ts

--- src/services/StudentService.ts
+++ src/services/StudentService.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://dv-student-backend-2019.appspot.com/',
+  baseURL: 'https://my-json-server.typicode.com/6zbSamuel/ComponentBasedlab2',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
