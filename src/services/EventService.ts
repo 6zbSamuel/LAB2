5437eedef61dfdd485951031ef0d13367fa19151
6zbSamuel <lzb2583842928@outlook.com>
2024-10-30 21:59:55 +0800

    6

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,16 +1,16 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/6zbSamuel/ComponentBasedlab2',
+  baseURL: 'https://my-json-server.typicode.com/mylove111/ComponentBasedlab2',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
+    'Content-Type': 'application/json'
+  }
 })
 
 export default {
   getEvents() {
     return apiClient.get('/events')
-  },
+  }
 }
